1c974238b434832c176a0fddf1def12ef633faca
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-08-23 16:41:26 -0400

    Status.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import csv
 import os
 import matplotlib.pyplot as plt
 
+number_of_applications = []
+
 application_info = []
 
 company = application_info.append(input("What company did you apply to? "))
@@ -10,9 +12,16 @@ date_year = application_info.append(int(input("Year: ")))
 date_month = application_info.append(int(input("Month: ")))
 date_day = application_info.append(int(input("Day: ")))
 
+status = int(input("What is the status of your application: \t (1) Applied (2) Rejected (3) Interview (4) Offer (5) No Response"))
+
+
+number_of_applications.append(application_info)
+
+len(number_of_applications)
+
 desktop_path = os.path.expanduser('~')+'\\Desktop\\coop-application-tracker.csv'
 
-with open('coop-application-tracker.csv', 'w') as f:
+with open('coop-application-tracker.csv', 'a') as f:
     #create the csv writer
     writer = csv.writer(f)
     #write a row to the csv file
