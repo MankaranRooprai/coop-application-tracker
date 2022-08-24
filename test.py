7fbe0f2e3bfb12c8258d994ec6b7ea4b23e1be3a
Mujtaba Zaidi <mujtabazaidi2003@gmail.com>
2022-08-24 12:26:37 -0400

    Finished Project!

--- test.py
+++ test.py
@@ -13,24 +13,22 @@ cont = "yes"
 desktop_path = os.path.expanduser('~')+'\\Desktop\\coop-application-tracker.csv'
 file_exists = os.path.exists(desktop_path)
 
-def status(csvreader, stati):
-    for row in csvreader:
-        if csvreader[3] == 1:
-            stati[0] += 1
-        elif csvreader[3] == 2:
-            stati[1] += 1
-        elif csvreader[3] == 3:
-            stati[2] += 1
-        elif csvreader[3] == 4:
-            stati[3] += 1
-        elif csvreader[3] == 5:
-            stati[4] += 1
-
 new_entry.clear()
 if file_exists:
     file = open(desktop_path)
     csvreader = list(csv.reader(file))
-    status(csvreader, stati)
+    for row in csvreader:
+        if row[3] == '1':
+            stati[0] += 1
+        elif row[3] == '2':
+            stati[1] += 1
+        elif row[3] == '3':
+            stati[2] += 1
+        elif row[3] == '4':
+            stati[3] += 1
+        elif row[3] == '5':
+            stati[4] += 1
+        csvreader.remove([])
     file.close()
 
 file = open(desktop_path, 'a')
@@ -40,16 +38,23 @@ while cont == "yes":
     new_entry.append(input("What company did you apply to? "))
     new_entry.append(input("Where is this position located? "))
     new_entry.append(input("When did you apply here? "))
-    new_entry.append(int(input("What is the status of your application: \t (1) Applied (2) Rejected (3) Interview (4) Offer (5) No Response \t")))
+    new_entry.append(input("What is the status of your application: \t (1) Applied (2) Rejected (3) Interview (4) Offer (5) No Response \t"))
     writer = csv.writer(file)
     writer.writerow(new_entry)
-    status(new_entry, stati)
+    if new_entry[3] == '1':
+        stati[0] += 1
+    elif new_entry[3] == '2':
+        stati[1] += 1
+    elif new_entry[3] == '3':
+        stati[2] += 1
+    elif new_entry[3] == '4':
+        stati[3] += 1
+    elif new_entry[3] == '5':
+        stati[4] += 1
     cont = input("Do you want to add another entry? \t (type 'yes' or 'no') ")
 file.close()
 
-newl = [a/4 for a in stati]
-
 responsevar = ["Applied", "Rejected", "Interview", "Offer", "No Response"]
 fig, ax = plt.subplots()
-ax.barh(responsevar, newl, color = ["blue", "Red", "cyan", "green", "black"])
+ax.barh(responsevar, stati, color = ["blue", "Red", "cyan", "green", "black"])
 plt.show()
